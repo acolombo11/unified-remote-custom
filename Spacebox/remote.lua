c6b33ba4f7dbb4dae8d228a8d675ee801d6dd06c
Andrea Colombo <andread251@gmail.com>
2017-06-27 16:02:13 +0200

    Open other remotes

--- Spacebox/remote.lua
+++ Spacebox/remote.lua
@@ -22,7 +22,35 @@ actions.open_netflix = function()
 end
 
 actions.open_steam = function()
-	os.start("steam");
+	os.start("steam:");
+end
+
+actions.close_firefox = function()
+	win.quit("steam.exe");
+end
+
+actions.close_steam = function()
+	win.quit("firefox.exe");
+end
+
+--@help Launch the Steam Remote
+actions.remote_steam = function()
+	dev.switch("Unified.SteamBasic");
+end
+
+--@help Launch the Firefox Remote
+actions.remote_firefox = function()
+	dev.switch("Unified.Firefox");
+end
+
+--@help Launch the Steam Monitor
+actions.remote_monitor = function()
+	dev.switch("Unified.Monitor");
+end
+
+--@help Switch Steam as the active window, then press ALT+Enter, which is the shortcut to Big Picture Mode
+	win.switchtowait("steam.exe");
+	kb.stroke("alt", "enter");
 end
 
 --@help Switch Firefox as the active window, then press F11, which is the shortcut to full-screen
